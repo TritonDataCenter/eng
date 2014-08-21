9154eb28692027ca49b8f6cdc2cccb246ff9f8af
Robert Mustacchi <rm@joyent.com>
2014-08-21 16:07:05 -0700

    TOOLS-603 eng.git should know about the new copyright bits

---
+++ prototypes/prototype.h
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc. All rights reserved.
+ */
+
+#ifndef _PROTOTYPE_H
+#define	_PROTOTYPE_H
+
+/*
+ * Describe the purpose of the file here.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Put your definitions in here */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PROTOTYPE_H */
