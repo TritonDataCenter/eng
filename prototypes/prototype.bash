7d19341053519e0aa2377dbd69d0c4e213233a6c
Tim Foster <tim.foster@joyent.com>
2020-01-06 17:09:42 +0000

    TOOLS-2399 bump eng.git prototypes for 2020 (#28)
    Reviewed by: Trent Mick <trentm@gmail.com>

--- prototypes/prototype.bash
+++ prototypes/prototype.bash
@@ -6,7 +6,7 @@
 #
 
 #
-# Copyright 2019 Joyent, Inc.
+# Copyright 2020 Joyent, Inc.
 #
 
 #
--- prototypes/prototype.rs
+++ prototypes/prototype.rs
@@ -2,6 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Copyright 2019 Joyent, Inc.
+// Copyright 2020 Joyent, Inc.
 
 //! Describe the purpose of this file.
